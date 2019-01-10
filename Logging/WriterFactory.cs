e41fd0771574410946f200bd517e67a77d3204d8
Alin <somalia97@yahoo.com>
2019-01-10 18:45:25 +0200

    Update XMLWriter

--- Logging/WriterFactory.cs
+++ Logging/WriterFactory.cs
@@ -9,12 +9,13 @@
         {
             switch (type)
             {
-                //case WriterTypes.CsvWriter:
-                //    return new CsvWriter(fileName);
-                //case WriterTypes.XmlWriter:
-                //    //Implement XMLWriter
-                //case WriterTypes.JsonWriter:
-                //    //Implement JSONWriter
+                case WriterTypes.CsvWriter:
+                    return new CsvWriter(fileName);
+                case WriterTypes.XmlWriter:
+                    return new XMLWriter(fileName);
+
+              //  case WriterTypes.JsonWriter:
+                    //Implement JSONWriter
                 default:
                     return new CsvWriter(fileName);
             }
